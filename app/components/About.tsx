7282550f9822ce9dc5cecd1813c68dca74a39b7a
ben <ndaloben8@gmail.com>
2025-02-05 17:39:27 +0300

    update about section title and layout

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { PlayCircle } from "lucide-react";
+import DottedRectangle from "./DottedRectangle";
 
 const About = () => {
     // Data for the About section
@@ -18,14 +19,14 @@ const About = () => {
                 text: "About Us",
                 href: "#services",
                 className:
-                    "px-6 py-2 text-white bg-gradient-to-r from-blue-600 to-purple-600 rounded-full font-semibold shadow-lg transition-transform hover:scale-105 hover:shadow-xl",
+                    "px-6 py-2 text-white dark:text-[#0D0D0D] bg-[#377DFF] dark:bg-[#E2E2E2] rounded-full font-semibold shadow-lg transition-transform hover:scale-105 hover:shadow-xl",
             },
             {
                 text: "Our Story",
                 href: "#contact",
                 icon: <PlayCircle className="w-5 h-5" />,
                 className:
-                    "flex gap-2 items-center px-6 py-2 text-blue-600 dark:text-white border border-blue-600 dark:border-gray-300 rounded-full font-semibold shadow-lg transition-transform hover:bg-blue-600 hover:text-white hover:scale-105 hover:shadow-xl",
+                    "flex gap-2 items-center px-6 py-2 dark:text-[#E2E2E2] bg-transparent border-[1px] dark:border-[#E2E2E2] border-[#377DFF] text-[#377DFF] rounded-full font-semibold shadow-lg transition-transform hover:scale-105 hover:shadow-xl",
             },
         ],
     };
@@ -56,11 +57,22 @@ const About = () => {
                 initial="hidden"
                 whileInView="visible"
                 variants={fadeInUp}
-                viewport={{ amount: 0.5 }} // Trigger when 50% of the element is in view
-                className="text-slate-800 dark:text-slate-200 capitalize drop-shadow-lg font-semibold"
+                viewport={{ amount: 0.5 }}
+                className="text-[#1D1D1D] dark:text-slate-200 capitalize drop-shadow-lg font-semibold"
             >
                 {aboutData.title}
             </motion.h1>
+
+            {/* Company Name */}
+            <motion.h2
+                initial="hidden"
+                whileInView="visible"
+                variants={fadeInUp}
+                viewport={{ amount: 0.5 }} // Trigger when 50% of the element is in view
+                className="text-3xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r dark:from-slate-400 dark:to-slate-600 from-slate-700 to-slate-900 mb-4"
+            >
+                About {aboutData.companyName}
+            </motion.h2>
 
             {/* Content Container */}
             <div className="flex flex-col md:flex-row items-center justify-center gap-4">
@@ -72,6 +84,13 @@ const About = () => {
                     viewport={{ amount: 0.5 }} // Trigger when 50% of the element is in view
                     className="relative w-full md:w-1/2"
                 >
+                    <div className="absolute -z-10 -top-12 -left-[105px]">
+                        <DottedRectangle rows={6} cols={8} width="250px" height="120px" gap="6px" color="#FF6E1A" />
+                    </div>
+                    <div className="absolute -z-10 -bottom-12 -right-[105px]">
+                        <DottedRectangle rows={6} cols={8} width="250px" height="120px" gap="6px" color="#EC1AFF" />
+                    </div>
+
                     <div className="relative mx-auto">
                         <Image
                             src="/images/about.jpg"
@@ -95,16 +114,7 @@ const About = () => {
                     viewport={{ amount: 0.5 }} // Trigger when 50% of the element is in view
                     className="md:max-w-2xl text-center md:text-left"
                 >
-                    {/* Company Name */}
-                    <motion.h2
-                        initial="hidden"
-                        whileInView="visible"
-                        variants={fadeInUp}
-                        viewport={{ amount: 0.5 }} // Trigger when 50% of the element is in view
-                        className="text-3xl sm:text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r dark:from-slate-400 dark:to-slate-600 from-slate-700 to-slate-900 mb-4"
-                    >
-                        About {aboutData.companyName}
-                    </motion.h2>
+
 
                     {/* Description */}
                     {aboutData.description.map((paragraph, index) => (
